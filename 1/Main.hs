e94916100248e5b49ba515475260f1dc284178af
Felix Springer <felixspringer149@gmail.com>
2020-12-04 19:23:37 +0100

    ez part 2

--- 1/Main.hs
+++ 1/Main.hs
@@ -3,6 +3,8 @@
 main :: IO ()
 main = do file <- readFile "./data"
           let inputData = read @ Integer <$> lines file
-              solution = filter ((/=) 0) $ [ if x + y == 2020 then x * y else 0 | x <- inputData, y <- inputData ]
-          print solution
+              solution1 = filter ((/=) 0) $ [ if x + y == 2020 then x * y else 0 | x <- inputData, y <- inputData ]
+          print solution1
+          let solution2 = filter ((/=) 0) $ [ if x + y + z == 2020 then x * y * z else 0 | x <- inputData, y <- inputData, z <- inputData ]
+          print solution2
           return ()
