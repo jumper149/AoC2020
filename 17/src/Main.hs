4861e09303fadb710a67afaa5731812df3045739
Felix Springer <felixspringer149@gmail.com>
2021-01-08 01:13:05 +0100

    part 1 ez

--- 17/src/Main.hs
+++ 17/src/Main.hs
@@ -5,10 +5,6 @@ import qualified Data.Set as S
 import Text.Parsec
 import Text.Parsec.String
 
-data Activity = Active
-              | Inactive
-              deriving (Bounded, Enum, Eq, Ord, Read, Show)
-
 data Coordinate = Coordinate { xCoordinate :: Integer
                              , yCoordinate :: Integer
                              , zCoordinate :: Integer
@@ -35,11 +31,53 @@ dataParser = do
               coords :: [[Coordinate]]
               coords = [ [ Coordinate x y 0 | x <- [ 0 .. ] ] | y <- [ 0 .. ] ]
 
+neighbors :: Coordinate -> [Coordinate]
+neighbors c@(Coordinate x y z) = do
+    x' <- neighborsInteger x
+    y' <- neighborsInteger y
+    z' <- neighborsInteger z
+    let c' = Coordinate x' y' z'
+    filter (c /=) $ pure c'
+        where neighborsInteger :: Integer -> [Integer]
+              neighborsInteger i = [ pred i, i, succ i]
+
+countActiveNeighbors :: ActiveCubes -> Coordinate -> Integer
+countActiveNeighbors cubes cube =
+    toEnum $ length $ filter isActive $ neighbors cube
+        where isActive :: Coordinate -> Bool
+              isActive c = c `S.member` cubes
+
+activateActiveCube :: ActiveCubes -> Coordinate -> Bool
+activateActiveCube cubes cube =
+    case countActiveNeighbors cubes cube of
+      2  -> True
+      3  -> True
+      _  -> False
+
+activateInactiveCube :: ActiveCubes -> Coordinate -> Bool
+activateInactiveCube cubes cube =
+    case countActiveNeighbors cubes cube of
+      3  -> True
+      _  -> False
+
+updateCubes :: ActiveCubes -> ActiveCubes
+updateCubes actives = fromActive <> fromInactive
+    where fromActive = S.filter (activateActiveCube actives) actives
+          fromInactive = S.filter (activateInactiveCube actives) considerableInactives
+          considerableInactives = S.fromList (concat $ neighbors <$> S.toList actives) S.\\ actives
+
+times :: (a -> a) -> a -> Int -> a
+times _ x 0 = x
+times f x n
+  | n < 0 = undefined
+  | otherwise = f $ times f x $ pred n
+
 main :: IO ()
 main = do
     inputData <- parseFromFile dataParser "./data"
     case inputData of
       Left err -> print err
-      Right dat -> print dat
-    print inputData
+      Right cubes -> do
+          let cubes' = times updateCubes cubes 6
+          print $ S.size cubes'
     return ()
